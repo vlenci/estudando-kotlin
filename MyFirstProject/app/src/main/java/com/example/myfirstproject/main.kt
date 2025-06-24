d6f86ce18ba211e5fae191d86d7ae0560a16d53a
Vinícius Lenci <vinicius.lenci@semeq.com>
2025-06-24 14:19:33 -0300

    Hello World!

---
+++ MyFirstProject/app/src/main/java/com/example/myfirstproject/main.kt
@@ -0,0 +1,10 @@
+package com.example.myfirstproject
+
+fun main () {
+    var variavel_declarada = 10
+    var variavel_nao_declarada: Int // quando o valor não é declarado tem que ter o tipo
+
+
+
+    println("Hello wwwORLddd!")
+}
